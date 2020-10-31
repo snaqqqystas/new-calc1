93d0c8712ee1fed81be22ffb224a0d0c785224c2
snaqqqy51 <snaqqqy51@gmail.com>
2020-10-31 10:50:08 +0200

    Initial Commit

---
+++ new calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new calc
+//
+//  Created by SNQMACPRO on 31.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ new calcTests/new_calcTests.swift
@@ -0,0 +1,33 @@
+//
+//  new_calcTests.swift
+//  new calcTests
+//
+//  Created by SNQMACPRO on 31.10.2020.
+//
+
+import XCTest
+@testable import new_calc
+
+class new_calcTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
